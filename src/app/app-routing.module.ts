83ce63709aa4ba505161d4d6498cc066942f605e
Freddy Ramirez <framirez@ingeneo.local>
2020-05-08 01:11:32 -0500

    Cargue del Ejercicio 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Ejercicio1Component } from './ejercicio-1/ejercicio1.component';
+import { Ejercicio2Component } from './ejercicio-2/ejercicio2.component';
 
 const routes: Routes = [
-  { path: 'ejercicio-1', component: Ejercicio1Component }
+  { path: 'ejercicio-1', component: Ejercicio1Component },
+  { path: 'ejercicio-2', component: Ejercicio2Component },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 // Ejercicio 1
 import { Ejercicio1Component } from './ejercicio-1/ejercicio1.component';
 import { HeaderCarouselComponent } from './ejercicio-1/header/header-carousel.component';
 import { CarouselComponent } from './ejercicio-1/carousel/carousel.component';
-import { SlideComponent } from './ejercicio-1/carousel/slide/slide.component';
 import { SlidesComponent } from './ejercicio-1/carousel/slides/slides.component';
+import { SlideComponent } from './ejercicio-1/carousel/slide/slide.component';
+import { Ejercicio2Component } from './ejercicio-2/ejercicio2.component';
+import { GameControlComponent } from './ejercicio-2/game-control/game-control.component';
+import { NumeroParComponent } from './ejercicio-2/numero-par/numero-par.component';
+import { NumeroImparComponent } from './ejercicio-2/numero-impar/numero-impar.component';
+
 
 @NgModule({
   declarations: [
@@ -21,11 +28,16 @@ import { SlidesComponent } from './ejercicio-1/carousel/slides/slides.component'
     HeaderCarouselComponent,
     CarouselComponent,
     SlideComponent,
-    SlidesComponent
+    SlidesComponent,
+    GameControlComponent,
+    NumeroParComponent,
+    NumeroImparComponent,
+    Ejercicio2Component
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpClientModule, 
+    RouterModule,   
     AppRoutingModule,
     FormsModule,
     NgbModule
